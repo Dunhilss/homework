1d83f58185faf4b8e140926a766808cc563fa44b
Dunhilss <treborg28@gmail.com>
2023-12-19 19:31:12 +0200

    added search for maximum number

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 number3 = int(input("Enter third number: "))
+
+if number1 >= number2 and number1 >= number3:
+    print(f"Highest number is: {number1}")
+elif number2 >= number1 and number2 >= number3:
+    print(f"Highest number is: {number2}")
+elif number3 >= number1 and number3 >= number2:
+    print(f"Highest number is: {number3}")
+else: print(f"Try again...")
