4ada32283fba64d2645025f6344d1b87c4cc9a35
Dunhilss <treborg28@gmail.com>
2023-12-19 20:51:56 +0200

    second task

--- main.py
+++ main.py
@@ -19,3 +19,6 @@ elif number3 <= number1 and number3 <= number2:
 else: print(f"Try again...")
 
 print(f"The arithmetic mean: {(number1 + number2 + number3) / 3}")
+
+metrs = int(input("Enter number of meters:"))
+print(f"Miles: {metrs * 0.00062137119609836} \nInches: {metrs * 39.370078740157} \nYards: {metrs * 1.0936132983377}")
