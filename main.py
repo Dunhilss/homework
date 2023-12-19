efc1f188badc9604a2f992d146955ea3371f4edf
Dunhilss <treborg28@gmail.com>
2023-12-19 19:33:05 +0200

    added search for minimum number

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ elif number2 >= number1 and number2 >= number3:
 elif number3 >= number1 and number3 >= number2:
     print(f"Highest number is: {number3}")
 else: print(f"Try again...")
+
+if number1 <= number2 and number1 <= number3:
+    print(f"Highest number is: {number1}")
+elif number2 <= number1 and number2 <= number3:
+    print(f"Highest number is: {number2}")
+elif number3 <= number1 and number3 <= number2:
+    print(f"Highest number is: {number3}")
+else: print(f"Try again...")
